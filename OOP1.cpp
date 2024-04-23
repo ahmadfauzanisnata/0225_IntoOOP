d0bba23824c500d8cb3b834339b85095cdfe73a2
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-23 15:55:47 +0700

    membuat prosedur output

--- OOP1.cpp
+++ OOP1.cpp
@@ -6,4 +6,10 @@ class mahasiswa{
     string nama;
     int umur;
     string jurusan;
+
+    void output(){
+        cout << "Nama: " << nama << endl;
+        cout << "Umur: " << umur << endl;
+        cout << "Jurusan: " << jurusan << endl;
+            }
 }
